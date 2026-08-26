af6b18a617f6776effbb9c8d1d68ca4980a0c44f
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2026-08-26 15:49:56 -0300

    26

--- main.js
+++ main.js
@@ -30,14 +30,16 @@ function aumentaTamanho() {
 
 const campoSenha=document.querySelector(#campo-senha);
 const letrasMaiusculas='ABCDEFG';
+
 geraSenha();
 function geraSenha(){
+let senha ="";
 
 for (let i = 0;i < tamanhoSenha; i++) {
-  
 let numeroAleatorio = Math.random()*letrasMaiusculas.length;
 numeroAleatorio=Math.floor(numeroAleatorio);
 console.log(letrasMaiusculas[numeroAleatorio]);
+senha=senha+letrasMaiusculas[numeroAleatorio];
 }
 }
-campoSenha.value=letrasMaiusculas;
+campoSenha.value=senha;
