35ec2acac98260f00fa61e49bf56d327030521f7
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2026-08-26 16:20:20 -0300

    26

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ if(tamanhoSenha >1){
 
 function aumentaTamanho() {
     if(tamanhoSenha<20){
-        //tamanhoSenha = tamanhoSenha+1;  //aumenta de 1 o valor do tamanhoSenha
-        tamanhoSenha++;
+      tamanhoSenha = tamanhoSenha+1;  //aumenta de 1 o valor do tamanhoSenha
+        //tamanhoSenha++;
     }
   numeroSenha.textContent = tamanhoSenha;//atribui o tamanho Senha ao numeroSenha
   geraSenha();
